bf13d98928694362a1d7983cb3aac0e6d10b1ba6
RenanCruz7 <renancruz199@gmail.com>
2025-10-15 20:03:26 -0300

    Feat: Design Tático - Entendendo os domains services

--- src/main/java/br/com/alura/codechella/domain/evento/Evento.java
+++ src/main/java/br/com/alura/codechella/domain/evento/Evento.java
@@ -5,13 +5,29 @@ import br.com.alura.codechella.domain.ingresso.TipoIngresso;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class Evento {
+    private UUID uuid;
     private Categoria   categoria;
     private String descricao;
     private LocalDateTime data;
     private Endereco endereco;
     private List<TipoIngresso> tipoIngressos = new ArrayList<>();
+
+    public Evento(Categoria categoria, String descricao, LocalDateTime data, Endereco endereco, List<TipoIngresso> tipoIngressos) {
+        this.categoria = categoria;
+        this.descricao = descricao;
+        this.data = data;
+        this.endereco = endereco;
+        this.tipoIngressos = tipoIngressos;
+
+        gerarIdentificadorUnico();
+    }
+
+    private void gerarIdentificadorUnico() {
+        this.uuid = UUID.randomUUID();
+    }
 
     public void incluiNovoTipoDeIngressoAoEvento(TipoIngresso tipoIngresso) {
         this.tipoIngressos.add(tipoIngresso);
